641a1f60ada5d658d9c47e498049a56b77c081b8
Chris Merck <cmerck@klendathu>
2016-08-14 11:57:07 -0400

    Prominence peak finder

--- py/algomorse.py
+++ py/algomorse.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 
 BLOCKSIZE = 512
 FFTSIZE = 8192
+SPECTRAL_MEDIAN_FILTER = 51
+PROMINENCE_THRESH = 25 # in dB
 
 # We work in floats, initially scaled [-1.0,+1.0],
 #  to avoid fixed-point complications.
@@ -21,10 +23,13 @@ class Algomorse(object):
   def __init__(self):
     # fft size must be integer number of blocks
     assert(FFTSIZE % BLOCKSIZE == 0)
+    # spectral median filter size must be odd-length
+    assert(SPECTRAL_MEDIAN_FILTER % 2 == 1)
     self.blocks_per_fft = FFTSIZE/BLOCKSIZE
     self.block_buf = []
     self.block_i = 0
-    self.fft_bs = []
+    self.pwrs = []
+    self.peakss = []
 
   def input_block_cb(self,block):
     # block is byte array of BLOCKSIZE samples
@@ -32,13 +37,23 @@ class Algomorse(object):
     self.block_buf.append(block)
     # do an FFT about twice per second
     if self.block_i % self.blocks_per_fft == 0:
-      # make windowed copy of input
-      fft_a = np.concatenate(self.block_buf[self.block_i-self.blocks_per_fft:])
-      assert(FFTSIZE == len(fft_a))
-      fft_a *= np.hanning(FFTSIZE) 
-      fft_b = np.fft.rfft(fft_a)
-      self.fft_bs.append(fft_b)
+      self.find_sigs()
 
+  def find_sigs(self):
+    # make windowed copy of input
+    fft_a = np.concatenate(self.block_buf[self.block_i-self.blocks_per_fft:])
+    assert(FFTSIZE == len(fft_a))
+    fft_a *= np.hanning(FFTSIZE) 
+    # real fft
+    fft_b = np.fft.rfft(fft_a)
+    # compute power spectrum (in dB)
+    pwr = np.log(np.square(np.real(fft_b)))*10
+    # filter noise from spectrum
+    pwr = median_filter(pwr,SPECTRAL_MEDIAN_FILTER)
+    self.pwrs.append(pwr)
+    # find peaks
+    peaks = prominence(pwr,PROMINENCE_THRESH)
+    self.peakss.append(peaks)
 
 def block_u16_to_float(block):
   return np.array(block)/(float(2**15))
@@ -55,8 +70,39 @@ def median_filter(a,n):
     rv[i] = med
   return rv
 
-if __name__ == "__main__":
+def prominence(pwr,thresh):
+  peaks = []
+  absmin = np.min(pwr)
+  absmax = np.max(pwr)
+  lmin = absmax
+  rmin = absmax
+  maxv = absmin
+  maxi = 0
+  LEFT = -1
+  RIGHT = +1
+  state = LEFT
+  for i in range(len(pwr)):
+    v = pwr[i]
+    if state == LEFT:
+      lmin = min(lmin,v)
+      if v - lmin > thresh:
+        state = RIGHT
+        rmin = absmax
+    if state == RIGHT:
+      rmin = min(rmin,v)
+      if v > maxv:
+        maxv = v
+        maxi = i
+        rmin = v
+      if maxv - rmin > thresh:
+        peaks.append(maxi)
+        lmin = absmax
+        rmin = absmax
+        maxv = absmin
+        state = LEFT
+  return peaks
 
+if __name__ == "__main__":
   infilename = "../websdr_recording_start_2016-08-14T11-24-45Z_14034.7kHz.wav"
 
   inwave = wave.open(infilename,'r')
@@ -85,16 +131,12 @@ if __name__ == "__main__":
     block = block_u16_to_float(block)
     am.input_block_cb(block)
 
-  data = np.log(np.square(np.real(np.array(am.fft_bs))))
-  print data
-
-  fig,ax = plt.subplots()
-  #for i in range(len(data)):
-  #  ax.plot(data[i])
-  #
-  # testing 51 Hz median filter
-  ax.plot(median_filter(data[5],51))
-  plt.show()
+  for i in range(len(am.pwrs)):
+    fig,ax = plt.subplots()
+    ax.plot(am.pwrs[i])
+    for j in range(len(am.peakss[i])):
+      ax.axvline(x=am.peakss[i][j])
+    plt.show()
 
   """fig,ax = plt.subplots()
   ax.imshow(data)
